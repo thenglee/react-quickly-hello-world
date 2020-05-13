a45ebf02a706f819dac1b411198ecddd500918b6
Tan Theng Lee <thengleetan@gmail.com>
2020-05-13 22:37:05 +0800

    Render element stored in variable

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 // @format
-ReactDOM.render( /*#__PURE__*/React.createElement("h1", null, "Hello world!"), document.getElementById('content'));
+let helloWorldReactElement = /*#__PURE__*/React.createElement("h1", null, "Hello world!");
+
+class HelloWorld extends React.Component {
+  render() {
+    return /*#__PURE__*/React.createElement("div", null, helloWorldReactElement, helloWorldReactElement);
+  }
+
+}
+
+ReactDOM.render( /*#__PURE__*/React.createElement(HelloWorld, null), document.getElementById('content'));
--- jsx/script.jsx
+++ jsx/script.jsx
@@ -1,3 +1,16 @@
 // @format
 
-ReactDOM.render(<h1>Hello world!</h1>, document.getElementById('content'));
+let helloWorldReactElement = <h1>Hello world!</h1>;
+
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <div>
+        {helloWorldReactElement}
+        {helloWorldReactElement}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById('content'));
