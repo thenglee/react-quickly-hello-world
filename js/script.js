d141e455f422b80b82c4b7e81e252246e3efeed5
Tan Theng Lee <thengleetan@gmail.com>
2020-05-13 22:47:51 +0800

    Pass data to child components using props

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 // @format
-let helloWorldReactElement = /*#__PURE__*/React.createElement("h1", null, "Hello world!");
-
 class HelloWorld extends React.Component {
   render() {
-    return /*#__PURE__*/React.createElement("div", null, helloWorldReactElement, helloWorldReactElement);
+    return /*#__PURE__*/React.createElement("h1", this.props, "Hello ", this.props.frameworkName, " world!!!");
   }
 
 }
 
-ReactDOM.render( /*#__PURE__*/React.createElement(HelloWorld, null), document.getElementById('content'));
+ReactDOM.render( /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(HelloWorld, {
+  id: "ember",
+  frameworkName: "Ember.js",
+  title: "A framework for creating ambitious web applications."
+}), /*#__PURE__*/React.createElement(HelloWorld, {
+  id: "backbone",
+  frameworkName: "Backbone.js",
+  title: "Backbone.js gives structure to web applications..."
+}), /*#__PURE__*/React.createElement(HelloWorld, {
+  id: "angular",
+  frameworkName: "Angular.js",
+  title: "Superheroic JavaScript MVW Framework"
+})), document.getElementById('content'));
--- jsx/script.jsx
+++ jsx/script.jsx
@@ -1,16 +1,28 @@
 // @format
 
-let helloWorldReactElement = <h1>Hello world!</h1>;
-
 class HelloWorld extends React.Component {
   render() {
-    return (
-      <div>
-        {helloWorldReactElement}
-        {helloWorldReactElement}
-      </div>
-    );
+    return <h1 {...this.props}>Hello {this.props.frameworkName} world!!!</h1>;
   }
 }
 
-ReactDOM.render(<HelloWorld />, document.getElementById('content'));
+ReactDOM.render(
+  <div>
+    <HelloWorld
+      id="ember"
+      frameworkName="Ember.js"
+      title="A framework for creating ambitious web applications."
+    />
+    <HelloWorld
+      id="backbone"
+      frameworkName="Backbone.js"
+      title="Backbone.js gives structure to web applications..."
+    />
+    <HelloWorld
+      id="angular"
+      frameworkName="Angular.js"
+      title="Superheroic JavaScript MVW Framework"
+    />
+  </div>,
+  document.getElementById('content'),
+);
